2dcecea5611132e9e8926b8a95ccf49c5489a7ab
AnwarHossainSR <mahedianwar@gmail.com>
2021-02-03 07:37:24 +0600

    First Commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    //Fetch Data directly
+    function index()
+    {
+        return DB::select("select * from users");
+    }
+
+    //Fetch data with model
+    function getData()
+    {
+        return User::all();
+    }
+}
